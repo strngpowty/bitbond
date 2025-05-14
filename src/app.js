759455d2d34e43fcc7706e46629f781c6890bbd0
strngpowty <strngpowty@gmail.com>
2025-05-14 21:21:38 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get("/user", (req, res) => {
-    res.send({"firstName":"djc"})
-})
-
-app.post("/user", (req, res) => {
-    res.send("added to db")
-})
+app.use("/user", [(req, res, next)=> {
+    next()
+    console.log("v")
+    res.send("Route Handler 1")
+},
+(req, res, next)=> {
+    // res.send("Route Handler 2")
+    console.log("vvb")
+    next()
+},
+(req, res, next)=> {
+    // res.send("Route Handler 3")
+    console.log("vvb")
+    // next()
+}])
 
 app.listen(3000)
