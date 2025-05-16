25d6cc6811d7032497acb59daa08c29b4cdf1c69
strngpowty <strngpowty@gmail.com>
2025-05-16 12:45:04 +0530

    WIP: Setup DB and Inserted data in collection

--- src/app.js
+++ src/app.js
@@ -1,20 +1,46 @@
 const express = require('express')
+const cors = require('cors')
+const connectDB = require("./config/database")
+const User = require("./models/user")
 const app = express()
 
-app.use("/user", [(req, res, next)=> {
-    next()
-    console.log("v")
-    res.send("Route Handler 1")
-},
-(req, res, next)=> {
-    // res.send("Route Handler 2")
-    console.log("vvb")
-    next()
-},
-(req, res, next)=> {
-    // res.send("Route Handler 3")
-    console.log("vvb")
-    // next()
-}])
+app.use(express.json())
+app.use(cors())
 
-app.listen(3000)
+app.post("/signup", async (req,res) => {
+    console.log(req.body)
+    // creating a new instance of the user model
+    const userObj = new User(req.body)
+    try{
+        await userObj.save();
+        res.send("User Added Successfully")
+    } catch(err) {
+        res.status(400).send("Adding User Failed")
+    }
+})
+
+// get single user -> by email
+app.get("/user", async (req, res) => {
+    const userEmail =  req.body.emailId
+    try{
+        const user = await User.find({emailId : userEmail})
+        res.send(user)
+    } catch(err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// get all users from db
+app.get("/feed", ()=>{
+
+});
+
+connectDB()
+.then(() => {
+    console.log("Database connected")
+    app.listen(3000, () =>{
+        console.log("running at 3000")
+    })
+}).catch(err => {
+    console.log("database connection failed")
+}) 
---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    emailId: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    age: {
+        type: String
+    },
+    gender: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model("User" , UserSchema)
