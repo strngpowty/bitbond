aabf1525716653c1cb9c2173b1702b343ac42c93
strngpowty <strngpowty@gmail.com>
2025-05-31 05:42:23 +0530

    Random Default Image Logic Added

--- src/models/user.js
+++ src/models/user.js
@@ -1,96 +1,100 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt") 
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 50,
-        // index : true ///  or create index like this 
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+      // index : true ///  or create index like this
     },
     lastName: {
-        type: String
+      type: String,
     },
     emailId: {
-        type: String,
-        required: true,
-        unique: true,  // automatically creates index when field set to unique : true
-        lowercase:true,
-        trim: true,
-        validate(value){
-            if (!validator.isEmail(value)) {
-                throw new Error("Invalid Email Address "+ value)
-            }
+      type: String,
+      required: true,
+      unique: true, // automatically creates index when field set to unique : true
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email Address " + value);
         }
+      },
     },
     password: {
-        type: String,
-        required: true,
-        validate(value){
-            if (!validator.isStrongPassword(value)) {
-                throw new Error("Invalid Password "+ value)
-            }
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Invalid Password " + value);
         }
+      },
     },
     age: {
-        type: String,
-        min: 18
+      type: String,
+      min: 18,
     },
     gender: {
-        type: String,
-        enum: {
-            values : ["male", "female", "others"],
-            message : `{VALUE} is not a valid gender type`
-        },
-        validate(value) {
-            if(!["male", "female", "others"].includes[value]) {
-                throw new Error("Gender data is not valid")
-            }
+      type: String,
+      enum: {
+        values: ["male", "female", "others"],
+        message: `{VALUE} is not a valid gender type`,
+      },
+      validate(value) {
+        if (!["male", "female", "others"].includes[value]) {
+          throw new Error("Gender data is not valid");
         }
+      },
     },
     photoURL: {
-        type: String, 
-        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwW1e1kKTLfRPF26r5KM8k7pC2ADEzvq5wzdo94Te4RcalfrStUuF48SwkBDT71986g7U&usqp=CAU",
-        validate(value){
-            if (!validator.isURL(value)) {
-                throw new Error("Invalid Image Address "+ value)
-            }
+      type: String,
+      default: function () {
+        const seed = Math.random().toString(36).substring(2, 15);
+        return `https://api.dicebear.com/7.x/adventurer/svg?seed=${seed}`;
+      },
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Image Address " + value);
         }
+      },
     },
-    about:{
-        type: String
+    about: {
+      type: String,
     },
     skills: {
-        type: [String],
-        default: ["Googling"]
-    }
-}, 
-{
-    timestamps: true
-})
-
+      type: [String],
+      default: ["Googling"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 /// jwt token created here - attached to user schema
-UserSchema.methods.getJWT = async function() {
-    const user = this;
-    const token = await jwt.sign({_id : user._id}, "Admin12!@", {
-        expiresIn: "1d"
-      })
-      return token;
-}
+UserSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, "Admin12!@", {
+    expiresIn: "1d",
+  });
+  return token;
+};
 
 /// offloading bcrypt in schema
-UserSchema.methods.validatePassword = async function(passwordInputByUser) {
-    const user = this;
-    const passwordHash = user.password
-    const isPasswordValid = await bcrypt.compare(
-        passwordInputByUser, 
-        passwordHash
-    );
-    return isPasswordValid
-}
+UserSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+  return isPasswordValid;
+};
 
-module.exports = mongoose.model("User" , UserSchema)
+module.exports = mongoose.model("User", UserSchema);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -38,7 +38,7 @@ authRouter.post("/login", async(req, res) => {
           const token = await user.getJWT();        
           res.cookie("token", token, {expires : new Date(Date.now() + 8 * 3600000)})
 
-          res.send("Login Successfull")
+          res.send(user)
         } else {
           throw new Error("Invalid Password")
         }
